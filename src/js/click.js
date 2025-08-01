2bba94404952411a405f2a89cab956191f23f3ec
Petro Lungu <polo200875@gmail.com>
2025-08-01 09:11:19 +0200

    added index aboutus styles

---
+++ src/js/click.js
@@ -0,0 +1,33 @@
+    //  document.addEventListener("DOMContentLoaded", function () {
+    //     const navIcon = document.querySelector(".nav__icon");
+    //     const nav = document.querySelector(".header");
+
+
+    //     navIcon.addEventListener("click", function () {
+    //       nav.classList.toggle("active");
+    //     });
+//  });
+    (function () {
+    const isOpen = document.querySelector(".nav__icon");
+    const isClose = document.querySelector(".nav__icon-close");
+    const navigation = document.querySelector(".header");
+
+    if (isOpen && navigation) {
+        isOpen.addEventListener("click", () => {
+            navigation.classList.toggle("active");
+        });
+    }
+
+    if (isClose && navigation) {
+        isClose.addEventListener("click", () => {
+            navigation.classList.remove("active");
+        });
+    }
+
+    document.addEventListener("click", (event) => {
+        if (navigation && !navigation.contains(event.target) && isOpen && !isOpen.contains(event.target)) {
+            navigation.classList.remove("active");
+        }
+    });
+})();
+      
---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+// js
+import '../js/click.js';
+import '../js/tabs.js';
+import Swiper from 'swiper';
+import { Pagination} from 'swiper/modules';
+
+// css
+import 'swiper/css';
+import 'swiper/css/pagination';
+import '../scss/style.scss';
+
+
+const swiper = new Swiper('.swiper', {
+  modules: [Pagination],
+  pagination: {
+      el: '.swiper-pagination',
+  },
+});
